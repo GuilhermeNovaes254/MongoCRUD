294db33d365a988ad8cca1184af7338304de0f75
GuilhermeNovaes254 <guilherme.novaes@gmail.com>
2020-08-20 15:35:59 -0300

    Finding params for updateById

--- server.js
+++ server.js
@@ -2,24 +2,32 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
-app.use(bodyParser.urlencoded({ extended:true}))
+const ObjectId = require('mongodb').ObjectID;
+const MongoClient = require('mongodb').MongoClient;
+
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
 app.set('view engine', 'ejs');
 
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://crud-node:<password>@cluster0.kxcxw.mongodb.net/<namedb>?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// const uri = "mongodb+srv://crud-node:<password>@cluster0.kxcxw.mongodb.net/<namedb>?retryWrites=true&w=majority";
+const uri = "mongodb://localhost:27017/names"
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 
 client.connect(err => {
-    if(err) return console.log(err)
+    if (err) return console.log(err)
     db = client.db('names')
     console.log("Creating Connection")
     app.listen(3000, function () {
         console.log("server running on port 3000");
     })
 });
-
-
 
 app.get('/', function (req, res) {
     res.render('index.ejs');
@@ -31,7 +39,7 @@ app.post('/show', function (req, res) {
 
         console.log('Saved in the database.')
         res.redirect('/show')
-        db.collection('data').find().toArray((err, results)=>{
+        db.collection('data').find().toArray((err, results) => {
             console.log(results);
         })
     })
@@ -41,8 +49,34 @@ app.post('/show', function (req, res) {
 app.get('/show', (req, res) => {
     db.collection('data').find().toArray((err, results) => {
         if (err) return console.log(err)
-        res.render('show.ejs', { data: results })
+        res.render('show.ejs', {
+            data: results
+        })
 
     })
 })
 
+app.get('/delete', (req, res) => {
+    let name = req.query.name
+    console.log(name)
+    db.collection('data').deleteOne({
+        name: name
+    })
+
+    res.redirect('/show')
+})
+
+app.get('/update', (req, res) => {
+    let {
+        name,
+        surname,
+        id
+    } = req.query
+    db.collection('data').findOne({_id: ObjectId(id)}, ((err, results) => {
+
+        console.log(results)
+        console.log(id)
+
+        res.redirect('/show')
+    }))
+})
